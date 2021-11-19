9ed388cf223065f4bf31ffff2977acacc080d456
Ante Wall <antewall@gmail.com>
2021-11-19 13:52:32 +0000

    randomise random bonus number if normal numbers is full #8

--- src/commands/random.ts
+++ src/commands/random.ts
@@ -1,10 +1,22 @@
 import { Response } from 'express';
+import moment from 'moment';
+import { CustomLotteryRow } from '../database';
+import { saveCustomBonus } from './saveCustomBonus';
 import { saveCustomNumber } from './saveCustomNumber';
 
 export async function randomCommand(response: Response, responseUrl: string, team: string) {
     const highest = 50;
     const number: number = Math.floor(Math.random() * highest) + 1;
+
+    let lotteryRow = await CustomLotteryRow.findOne({
+        team_id: team,
+        week: moment().isoWeek(),
+        year: moment().year(),
+    });
+    if (lotteryRow && lotteryRow.numbers.length >= 5) {
+        // @ts-ignore
+        return saveCustomBonus(response, { text: { text: number.toString() } }, responseUrl, team)
+    }
     // @ts-ignore
-    return saveCustomNumber(response, { text: {text: number.toString() } }, responseUrl, team)
+    return saveCustomNumber(response, { text: { text: number.toString() } }, responseUrl, team)
 }
-  
